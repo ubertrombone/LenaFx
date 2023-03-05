43ef745b0a0699ac18e491707a98e8eb5319bcef
ubertrombone <josh_rose@alumni.baylor.edu>
2023-03-05 20:25:09 +0200

    Refactors TopBar.kt and adds NavigationIconUtil.kt

---
+++ app/src/main/java/com/roseFinancials/lenafx/ui/composables/scaffold/NavigationIconUtil.kt
@@ -0,0 +1,52 @@
+package com.roseFinancials.lenafx.ui.composables.scaffold
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavBackStackEntry
+import com.roseFinancials.lenafx.destinations.Destination
+import com.roseFinancials.lenafx.destinations.DividendsScreenDestination
+import com.roseFinancials.lenafx.destinations.StocksScreenDestination
+import com.roseFinancials.lenafx.stocks.DividendsViewModel
+import com.roseFinancials.lenafx.stocks.StocksViewModel
+import com.roseFinancials.lenafx.utils.ResetViewModel
+
+@Composable
+fun NavigationIconUtil(
+    destination: Destination,
+    navBackStackEntry: NavBackStackEntry?,
+    onClick: () -> Unit
+) {
+    val viewModel = destination.getViewModelInstance(navBackStackEntry)
+    viewModel?.let {
+        IconButton(onClick = {
+            viewModel.resetState()
+            onClick()
+        }) {
+            Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = "Previous Page"
+            )
+        }
+    }
+}
+
+@Composable
+fun Destination.getViewModelInstance(
+    navBackStackEntry: NavBackStackEntry?
+): ResetViewModel? {
+    return when (this) {
+        StocksScreenDestination -> {
+            navBackStackEntry?.let { hiltViewModel<StocksViewModel>(navBackStackEntry) }
+                ?: throw NullPointerException("Screen has no backstack")
+        }
+        DividendsScreenDestination -> {
+            navBackStackEntry?.let { hiltViewModel<DividendsViewModel>(navBackStackEntry) }
+                ?: throw NullPointerException("Screen has no backstack")
+        }
+        else -> null
+    }
+}
--- app/src/main/java/com/roseFinancials/lenafx/ui/composables/scaffold/TopBar.kt
+++ app/src/main/java/com/roseFinancials/lenafx/ui/composables/scaffold/TopBar.kt
@@ -1,21 +1,16 @@
 package com.roseFinancials.lenafx.ui.composables.scaffold
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import com.ramcosta.composedestinations.navigation.popUpTo
 import com.roseFinancials.lenafx.NavGraphs
 import com.roseFinancials.lenafx.R
 import com.roseFinancials.lenafx.destinations.Destination
-import com.roseFinancials.lenafx.destinations.StocksScreenDestination
-import com.roseFinancials.lenafx.stocks.StocksViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -26,21 +21,16 @@ fun TopBar(
 ) {
     TopAppBar(
         navigationIcon = {
-            destination.TopBarNavigationButton(
+            NavigationIconUtil(
+                destination = destination,
                 navBackStackEntry = previousBackStackEntry,
-                button = {
+                onClick = {
                     navController.navigate(previousBackStackEntry?.destination?.route!!) {
                         popUpTo(NavGraphs.root) {
                             saveState = false
                         }
                         restoreState = false
                     }
-                },
-                icon = {
-                    Icon(
-                        imageVector = Icons.Default.ArrowBack,
-                        contentDescription = "Previous Page"
-                    )
                 }
             )
         },
@@ -57,22 +47,4 @@ fun TopBar(
             navigationIconContentColor = MaterialTheme.colorScheme.onPrimary
         )
     )
-}
-
-@Composable
-fun Destination.TopBarNavigationButton(
-    navBackStackEntry: NavBackStackEntry?,
-    button: () -> Unit,
-    icon: @Composable () -> Unit
-) {
-    return when (this) {
-        StocksScreenDestination -> {
-            val viewModel = navBackStackEntry?.let { hiltViewModel<StocksViewModel>(navBackStackEntry) }
-            IconButton(onClick = {
-                viewModel?.resetState()
-                button()
-            }) { icon() }
-        }
-        else -> return
-    }
 }
